6d15d6a943fd3421709e6a033ed83db25c794b45
sstrabe <sstrabe09@gmail.com>
2025-01-13 15:11:56 +0100

    Add email fetch api (in progress not functioning)

--- src/api/emails/index.ts
+++ src/api/emails/index.ts
@@ -1,6 +1,19 @@
 import { Handler } from "express";
 import { createTransport } from "nodemailer"
-import { ImapFlow } from "imapflow";
+import { ImapFlow, MailboxObject } from "imapflow";
+import { ParsedMail, simpleParser } from "mailparser";
+import { parse } from "dotenv";
+
+function createArray(start: number, end: number) {
+    return Array.from({ length: end - start + 1 },
+        (_, index) => start + index);
+}
+
+const concat_RS = (stream: any): any => new Promise((res, rej) => {
+    let buffers: any[] = [];
+    stream.on("data", function (data: any) { buffers.push(data); });
+    stream.on("end", function () { res(Buffer.concat(buffers)); });
+});
 
 export const POST: Handler = async (req, res) => {
     if (!req.auth) return;
@@ -31,14 +44,14 @@ export const POST: Handler = async (req, res) => {
 export const GET: Handler = async (req, res) => {
     if (!req.auth) return;
 
-    const { inbox, limit } = req.body;
+    const { mailbox, limit } = req.body;
     const { email, password } = req.auth;
 
     const client = new ImapFlow({
         host: 'imap.gmail.com',
         port: 993,
         secure: true,
-        
+
         auth: {
             user: email,
             pass: password
@@ -48,9 +61,23 @@ export const GET: Handler = async (req, res) => {
     try {
         await client.connect();
 
-        const lock = await client.getMailboxLock(inbox);
-        
-    } catch(err) {
+        const lock = await client.getMailboxLock(mailbox);
+        try {
+            //@ts-ignore
+            const emails = await client.fetchAll(createArray(client.mailbox.exists - limit, client.mailbox.exists), {})
+            res.status(200).send({
+                data: emails.map(async (email) => {    
+                    let { meta, content } = await client.download(email.seq.toString());
+                    const contentString = (await concat_RS(content)).toString()
+                    const parsedMail: ParsedMail = await simpleParser(content)
+    
+                    return parsedMail
+                })
+            })
+        } finally {
+            lock.release()
+        }
+    } catch (err) {
 
     }
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,48 @@
 import './globals.css';
 import '../components/compose-email/index.js';
+import { ParsedMail } from 'mailparser';
+
+const emailList = document.querySelector('ul');
+const template = document.getElementById('email-list-template') as HTMLTemplateElement;
+
+function createEmailListItem(subject: string, sender: string, content: string) {
+    const clone = template.content.cloneNode(true) as HTMLElement;
+
+    const img = clone.querySelector('img') as HTMLImageElement;
+    const subjectElement = clone.querySelector('#subject') as HTMLSpanElement;
+    const bodyElement = clone.querySelector('#content') as HTMLParagraphElement;
+    const senderElement = clone.querySelector('#sender') as HTMLSpanElement;
+
+    subjectElement.textContent = subject
+    senderElement.textContent = sender
+    bodyElement.textContent = content.slice(0, 50)
+
+    emailList?.appendChild(clone)
+}
+
+const token = localStorage.getItem('token')?.split(';') as string[]
+
+if (new Date(token[1].split('=')[1]) < new Date()) {
+    alert('You are not logged in');
+    localStorage.removeItem('token');
+};
+
+const res = await fetch('/api/emails', {
+    method: 'GET',
+    body: JSON.stringify({
+        mailbox: 'INBOX',
+        limit: 10
+    }),
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token[0]}`
+    }
+})
+
+const body = await res.json()
+if ('data' in body) {
+    body.data.forEach(async (email: ParsedMail) => {
+        const { subject, from, text } = email
+        createEmailListItem(subject!, from?.text!, text!)
+    })
+}
